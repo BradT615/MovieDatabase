09026130af1e8636ded0d53dae5b0ee3aac5f263
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-06-11 02:25:25 -0400

    Added details page

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-//TMDB 
+// TMDB 
 const API_KEY = 'api_key=bfac323cd6003abb6795dd6ba81cf289';
 const BASE_URL = 'https://api.themoviedb.org/3/';
-
 const API_URL = BASE_URL + 'discover/movie?sort_by=popularity.desc&' + API_KEY + '&page=1&limit=24';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY;
+const SEARCH_URL = BASE_URL + 'search/movie?' + API_KEY;
+const MOVIE_DETAILS_URL = BASE_URL + 'movie/';
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
-
-getMovies(API_URL);
+// Check if we're not on details page
+if (!window.location.href.endsWith("details.html")) {
+    getMovies(API_URL);
+}
 
 function getMovies(url) {
     fetch(url)
@@ -27,9 +29,9 @@ function showMovies(data) {
     movieRow.innerHTML = '';
 
     data.forEach(movie => {
-        const { title, poster_path, vote_average, overview } = movie;
+        const { id, title, poster_path, vote_average, overview } = movie;
         const movieElement = document.createElement('div');
-        movieElement.classList.add('col-m-6', 'col-xl-3','p-0', 'movie');
+        movieElement.classList.add('col-m-6', 'col-xl-3', 'p-0', 'movie');
         movieElement.innerHTML = `
             <div class="movie">
                 <div class="movie-img">
@@ -45,15 +47,17 @@ function showMovies(data) {
                 </div>
             </div>
         `;
+        movieElement.addEventListener('click', () => {
+            localStorage.setItem('movie', id);
+            window.location.href = './details.html';
+        });
         movieRow.appendChild(movieElement);
     });
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const searchTerm = search.value;
-
     if (searchTerm) {
         getMovies(SEARCH_URL + '&query=' + searchTerm);
     } else {
@@ -61,12 +65,38 @@ form.addEventListener('submit', (e) => {
     }
 });
 
-//will add later
-// {/* <div class="overview">
-//     <div class="overview-title">
-//         <h3>Overview</h3>
-//     </div>
-//     <div class="overview-content">
-//         ${overview}
-//     </div>
-// </div> */}
+window.onload = function() {
+    if (window.location.href.endsWith("details.html")) {
+        const movieId = localStorage.getItem('movie');
+        const main = document.getElementById('main');
+
+        // Fetch movie details
+        fetch(`${MOVIE_DETAILS_URL}${movieId}?${API_KEY}`)
+        .then(response => response.json())
+        .then(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.classList.add('movie');
+
+            movieElement.innerHTML = `
+                <div class="movie-img">
+                    <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
+                </div>
+                <div class="movie-info">
+                    <div class="movie-title">
+                        <h3>${movie.title}</h3>
+                    </div>
+                    <div class="overview">
+                        <h3>Overview</h3>
+                        <p>${movie.overview}</p>
+                        <h3>Score: ${movie.vote_average}</h3>
+                    </div>
+                </div>
+            `;
+
+            main.appendChild(movieElement);
+        })
+        .catch(error => console.log(error));
+
+        localStorage.removeItem('movie');
+    }
+}
